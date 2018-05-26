373fe94760a7974640b5c1545f84a0e79bcb2535
Kevin <kevin.medzor@gmail.com>
2018-05-26 15:16:00 -0500

    Implemented the MIT license

--- src/Settings.java
+++ src/Settings.java
@@ -1,5 +1,22 @@
 
-import java.awt.Desktop;
+/*
+File:   Settings.java 
+Copyright 2018, Kevin Medzorian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,9 +29,6 @@ import java.util.Date;
 import java.util.Scanner;
 import org.jnativehook.keyboard.NativeKeyEvent;
 
-/**
- * @author Kevin
- */
 public class Settings {
 
     public static String os = "Windows";
